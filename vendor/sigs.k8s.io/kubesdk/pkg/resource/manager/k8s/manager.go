6a17a4566729d587aa4acb032cbc2c6d25a63902
hxquangnhat <nhat.hoang@deliveryhero.com>
2019-02-26 15:52:44 +0100

    add new func for k8s manager

--- vendor/sigs.k8s.io/kubesdk/pkg/resource/manager/k8s/manager.go
+++ vendor/sigs.k8s.io/kubesdk/pkg/resource/manager/k8s/manager.go
@@ -479,3 +479,8 @@ func Get(rm manager.Manager, nn types.NamespacedName, o runtime.Object) error {
 	krm := rm.(*RsrcManager)
 	return krm.client.Get(context.TODO(), nn, o)
 }
+
+// GetInstance - return existing instance list
+func (rm *RsrcManager) GetInstanceMap() map[string]resource.Item {
+	return nil
+}
